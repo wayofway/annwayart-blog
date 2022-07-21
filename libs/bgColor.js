7c05c5fcaae86940f60ab9aff2c26b52f20bf4a7
annwayli <annwayli@tencent.com>
2022-07-21 12:18:48 +0800

    Site updated: 2022-07-21 12:18:47

--- libs/bgColor.js
+++ libs/bgColor.js
@@ -1,1 +1,17 @@
-const COLOR_MAP={SUNNY:"#ffeaa7",RAINY:"#74b9ff",SNOWY:"#81ecec",WINDY:"#ffcccc",CLOUDY:"#dfe6e9"},WEATHER_MAP=["SUNNY","RAINY","SNOWY","WINDY","CLOUDY"];var nickNode,randomNum=(5*Math.random()-1).toFixed();0<=randomNum&&(nickNode=document.querySelector("#nickname"),document.body.style.backgroundColor=nickNode?COLOR_MAP.SUNNY:COLOR_MAP[WEATHER_MAP[randomNum]]);
+const COLOR_MAP = {
+  SUNNY: "#ffeaa7",
+  RAINY: "#74b9ff",
+  SNOWY: "#81ecec",
+  WINDY: "#ffcccc",
+  CLOUDY: "#dfe6e9",
+};
+const WEATHER_MAP = ["SUNNY", "RAINY", "SNOWY", "WINDY", "CLOUDY"];
+var randomNum = (Math.random() * 5 - 1).toFixed();
+if (randomNum >= 0) {
+  var nickNode = document.querySelector("#nickname");
+  if (nickNode) {
+    document.body.style.backgroundColor = COLOR_MAP.SUNNY;
+  } else {
+    document.body.style.backgroundColor = COLOR_MAP[WEATHER_MAP[randomNum]];
+  }
+}
--- libs/eightDiagrams.js
+++ libs/eightDiagrams.js
@@ -1,1 +1,61 @@
-const DIAGRAM_MAP={1:"乾",2:"兑",3:"离",4:"震",5:"巽",6:"坎",7:"艮",0:"坤"},CHANGE_MAP={1:"初",2:"二",3:"三",4:"四",5:"五",0:"上"},curDay=(new Date).getDate(),LastUpdatedDate=localStorage.getItem("last_updated_day")||"";let diagramCache=localStorage.getItem("diagram")||"",bot,up,change;if(diagramCache&&curDay==LastUpdatedDate){const{bottom:a,top:b,changes:c}=JSON.parse(diagramCache);bot=a,up=b,change=c}else{bot=(1e3*Math.random()).toFixed(),up=(1e3*Math.random()).toFixed(),change=(1e3*Math.random()).toFixed(),bot%=8,up%=8,change%=6;const d={bottom:bot,top:up,changes:change};localStorage.setItem("diagram",JSON.stringify(d)),localStorage.setItem("last_updated_day",curDay)}var originNum=`${up}-${bot}-${change}`,diagramWords=`${DIAGRAM_MAP[up]}上${DIAGRAM_MAP[bot]}下 变爻在${CHANGE_MAP[change]}`,searchWords=`${diagramWords} 易安居吉祥网`,analysisLink=`https://www.baidu.com/s?ie=utf-8&wd=${encodeURI(searchWords)}`;console.log(originNum,diagramWords,analysisLink);
+const DIAGRAM_MAP = {
+  1: "乾",
+  2: "兑",
+  3: "离",
+  4: "震",
+  5: "巽",
+  6: "坎",
+  7: "艮",
+  0: "坤",
+};
+
+const CHANGE_MAP = {
+  1: "初",
+  2: "二",
+  3: "三",
+  4: "四",
+  5: "五",
+  0: "上",
+};
+
+// 今天
+const curDay = new Date().getDate();
+// 缓存
+const LastUpdatedDate = localStorage.getItem("last_updated_day") || "";
+let diagramCache = localStorage.getItem("diagram") || "";
+
+let bot;
+let up;
+let change;
+
+if (diagramCache && curDay == LastUpdatedDate) {
+  // 有缓存无须更新
+  const { bottom, top, changes } = JSON.parse(diagramCache);
+  bot = bottom;
+  up = top;
+  change = changes;
+} else {
+  bot = (Math.random() * 1000).toFixed();
+  up = (Math.random() * 1000).toFixed();
+  change = (Math.random() * 1000).toFixed();
+
+  bot %= 8;
+  up %= 8;
+  change %= 6;
+
+  const diagram = { bottom: bot, top: up, changes: change };
+  localStorage.setItem("diagram", JSON.stringify(diagram));
+  localStorage.setItem("last_updated_day", curDay);
+}
+
+var originNum = `${up}-${bot}-${change}`;
+
+var diagramWords = `${DIAGRAM_MAP[up]}上${DIAGRAM_MAP[bot]}下 变爻在${CHANGE_MAP[change]}`;
+
+var searchWords = `${diagramWords} 易安居吉祥网`;
+
+var analysisLink = `https://www.baidu.com/s?ie=utf-8&wd=${encodeURI(
+  searchWords
+)}`;
+
+console.log(originNum, diagramWords, analysisLink);
